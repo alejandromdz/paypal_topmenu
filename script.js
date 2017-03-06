06a4f0645c8615a2c00a55ff6ddf0ee5a6cedfb1
Alejandro Mdz <alejandromdz@gmail.com>
2017-03-05 22:24:14 -0800

    main nav working

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-window.onload=function(){
+window.onload = function () {
 
-var mainNav=document.getElementById('main-nav');
-mainNav.onclick=function(){
-    this.classList.toggle('closed');
-    this.classList.toggle('opened');
-}
+    var mainNav = document.getElementById('main-nav');
+    var menuItems = document.querySelectorAll('.dropdown li');
+    var selectableNavs = document.querySelectorAll('.selectable-nav');
+
+    menuItems[0].onclick = function () {
+        menuItems[1].classList.remove('selected');
+        this.classList.toggle('selected');
+        if (document.querySelector('li.selected')) {
+            mainNav.classList.add('opened');
+            selectableNavs[0].classList.add('selected');
+            selectableNavs[1].classList.remove('selected');
+        }
+        else
+        { 
+            mainNav.classList.remove('opened'); 
+            selectableNavs[0].classList.remove('selected');
+        }
+    }
+    menuItems[1].onclick = function () {
+        menuItems[0].classList.remove('selected');
+        this.classList.toggle('selected');
+        if (document.querySelector('li.selected')) {
+            mainNav.classList.add('opened');
+            selectableNavs[1].classList.add('selected');
+            selectableNavs[0].classList.remove('selected');
+        }
+        else
+        { 
+            mainNav.classList.remove('opened'); 
+            selectableNavs[1].classList.remove('selected');
+        }
+    }
 
 };
